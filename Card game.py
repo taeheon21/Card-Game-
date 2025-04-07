3b6c9286c5e4ceb3106db316b6a5cbdd7074b1bc
taeheon21 <taeheon0121@gmail.com>
2025-04-07 21:58:32 +0100

    Add files via upload

---
+++ Card game.py
@@ -0,0 +1,39 @@
+import random
+
+class Game:
+    def __init__(self):
+        self.deck = Deck()
+        self.number_cards = []
+        self.figure_cards = []
+        self.split_deck()
+        self.players = [Player("Player 1"), Player("Player 2")]
+        self.deal_cards()
+
+    def check_winner(self):
+        # Automatic win (Player's score >= 91)
+        for player in self.players:
+            if player.score >= 91:
+                print(f"{player.name} wins")
+        # If there is no card anymore, decide by comparing scores or by flip a coin
+        if all(len(player.hand)) == 0 for player in self.players:
+
+            if self.players[1].score > self.players[2].score:
+                print(f"{player.1} wins")
+
+            elif self.players[1].score < self.players[2].score:
+                print(f"{player.2} wins")
+            #flip coin(else) I just use random I could make flip coin to decide winner.
+            else:
+                chosen = random.choice(self.players)
+                print(f"{chosen.name} wins by a coin flip")
+
+
+        # If there is no winner yet
+        return None
+
+
+
+
+
+
+
