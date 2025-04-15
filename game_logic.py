0e9aa7a1cce1f64d4aefe9dc69e1d8e3e0318eb2
taeheon21 <taeheon0121@gmail.com>
2025-04-15 22:33:03 +0100

    Update game_logic.py
    I made a function to start the game, another to play the round, another to check all roounds are done, and a function to check if the game is over and so on..
    
    (*** But I think I need to make flip coin logic to decide winner when they are tie)

--- game_logic.py
+++ game_logic.py
@@ -1,35 +1,3 @@
-"""
-i left this code just incase we can use it
-import random
-
-class Game:
-    def __init__(self):
-        
-
-    def check_winner(self):
-        # Automatic win (Player's score >= 91)
-        for player in self.players:
-            if player.score >= 91:
-                print(f"{player.name} wins")
-        # If there is no card anymore, decide by comparing scores or by flip a coin
-        if all(len(player.hand)) == 0 for player in self.players:
-
-            if self.players[1].score > self.players[2].score:
-                print(f"{player.1} wins")
-
-            elif self.players[1].score < self.players[2].score:
-                print(f"{player.2} wins")
-            #flip coin(else) I just use random I could make flip coin to decide winner.
-            else:
-                chosen = random.choice(self.players)
-                print(f"{chosen.name} wins by a coin flip")
-
-
-        # If there is no winner yet
-        return None
-
-"""
-
 from game_logic.deck import Deck
 from game_logic.player import Player
 
@@ -43,8 +11,65 @@ class Game:
         self.computer = Player("computer")
         self.rounds = 5 #this is new but it might make the game quicker to play (we dont need it)
 
+        def start_game(self):
+        """starts the game"""
+        print("Game is starting...")
+        while self.round <= self.total_rounds:
+            self.round += 1
+            print(f"Round {self.round}")
+            self.play_round()
 
-""" needs a function to start the game, another to play the round, another to check all roounds are done, and a function to check if the game is over. """
+
+
+
+    def play_round(self):
+        """play round, both player and computer will draw card and card values will be compared"""
+        card_player = self.deck.draw_card()
+        card_computer = self.deck.draw_card()
+
+        #If there is no card anymore
+        if card_player is None or card_computer is None:
+            print("There's no card to draw")
+            return
+
+        print(f"You played: {card_player}")
+        print(f"Computer played: {card_computer}")
+
+        # Comparing number
+        if card_player > card_computer:
+            print(f"You win {self.round}!")
+            self.player.add_score(card_player)
+        elif card_computer > card_player:
+            print(f"Computer win {self.round}!")
+            self.computer.add_score(card_computer)
+        # flip coin(else) I just use random I could make flip coin to decide winner.
+        else:
+            print(f"This round is tie! So We will decide winner by flip the coin")
+
+           # !!! I think we should make this flip coin logic /There are two coins for Player and for Computer. It will be randomly chosen  !!!
+            chosen = random.choice(self.coin)
+
+            print(f"{Chosen.name} win the game !")  # Just for now. I will change it 
+
+
+    def rounds_done(self):
+
+    def game_over(self):
+
+    def declare_winner(self):
+        """ declare final winner"""
+        print("Game Over!")
+        print(f"Your score is {self.player.score}")
+        print(f"Computer score is {self.computer.score}")
+        if self.player.score > self.computer.score:
+            print(f"Congratulations! You win the Game!!")
+        elif self.player.score < self.computer.score:
+            print(f"Computer wins the game")
+        else:
+            print(f"The game is tie! So We will decide winner by flip the coin")
+ 
+
+
 
 
 
