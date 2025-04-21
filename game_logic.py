37ba04c886189148879f0156efea90a010d72653
Alexandru Pascal <Alexandru.Pascal@warwick.ac.uk>
2025-04-21 22:43:59 +0300

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -9,8 +9,7 @@ class Game:
     def __init__(self):  # 5667929 wrote def __init part except self.round
         """initialize the game with a deck, two players, and a set number of rounds"""
         self.deck = Deck()
-        self.player = Player("you")
-        self.computer = Player("computer")
+        self.players = [Player("You"), Player("Computer")] #5676101
         self.round = 0  # 5662884 
         self.total_rounds = 5  # this is new but it might make the game quicker to play (we dont need it)
         self.user_round_sum = 0
@@ -25,10 +24,12 @@ class Game:
         self.declare_winner()
 
 
-    def play_round(self):
+    def play_round(self, user_card):
         """play round, both player and computer will draw card and card values will be compared"""
         card_player = self.deck.draw_card()
         card_computer = self.deck.draw_card()
+        user = self.players[0] #5676101
+        computer = self.players[1]
 
         #If there is no card anymore
         if card_player is None or card_computer is None:
@@ -58,7 +59,12 @@ class Game:
          # Special rule: no 3 or 10 right after using 2ofSp or 9ofSp 5676101
         if user.used_special == True and user_rank in ['3', '10']:
            raise ValueError("Error: You can't play a 3 or 10 right after using a special card like 2 of spades or 9 of spades")
-
+            #For each round the sum of the values of the number cards played will be stored (5676101)
+        rank_user = user.get_rank(user_card)
+        value_user = int(rank_user)
+        self.user_round_sum += value_user
+        if self.user_round_sum in [12, 19]: # If statement used to check if sum is 12 or 19
+           raise ValueError("You have reached an invalid card total of 12 or 19 this round.")
     def game_over(self):
         """check if game completion conditions are satisfied(round expired or deck is empty)"""
         return self.round >= self.total_rounds or self.deck.is_empty()
