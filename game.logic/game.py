ff679bdd4360405b5d5b585a5acf951d7999752f
Alexandru Pascal <Alexandru.Pascal@warwick.ac.uk>
2025-04-29 16:38:57 +0100

    Update game.py

--- game.logic/game.py
+++ game.logic/game.py
@@ -25,50 +25,64 @@ class Game:
         self.declare_winner()
 
 
-    def play_round(self, user_card):
+    def play_round(self):
         """play round, both player and computer will draw card and card values will be compared"""
-        card_player = self.deck.draw_card()
-        card_computer = self.deck.draw_card()
+        user_card = user.hand.pop(0)
+        computer_card = computer.hand.pop(0)
         user = self.players[0] #5676101
         computer = self.players[1]
-
+        rank_user = user.get_rank(user_card)
+        rank_computer = computer.get_rank(computer_card)
+        #The figure cards being drawn 5676101
+        figure_deck = self.deck.create_figure_cards()
+        if not figure_deck:
+            print("All figure cards have been exhausted!")
+            return
+        self.current_figure = figure_deck.popleft()
+        figure_id = self.current_figure[0]
+        figure_value = self.current_figure[1]
+        print(f"Figure card on the table: {figure_id} (worth {figure_value} points)")
         #If there is no card anymore
-        if card_player is None or card_computer is None:
+        if user_card is None or computer_card is None:
             print("There's no card to draw")
             return
 
-        print(f"You played: {card_player}")
-        print(f"Computer played: {card_computer}")
-
-        # Comparing number
-        if card_player > card_computer:
-            print(f"You win {self.round}!")
-            user.add_score(card_player)
-        elif card_computer > card_player:
-            print(f"Computer win {self.round}!")
-            computer.add_score(card_computer)
-        # flip coin(else) I  use random I could make flip coin to decide winner.
+        print(f"You played: {user_card}")
+        print(f"Computer played: {computer_card}")
+        #Enforcing the 2 of Spades and 9 of Spades rule
+        if user_card == '2S' or user_card == '9S':
+            user.used_special = True
+            user.last_special = user_card
         else:
-            print(f"This round is a tie! Deciding winner by coin flip...")
-            winner = random.choice([self.players[0], self.players[1])
-            if winner is self.players[0]:
-                print(f"You win {self.round} by coin flip!")
-                user.add_score(card_player)
-            else:
-                print(f"Computer wins {self.round} by coin flip!")
-                computer.add_score(card_computer)
+            user.used_special = False
          # Special rule: no 3 or 10 right after using 2ofSp or 9ofSp 5676101
         if user.used_special == True and rank_user in ['3', '10']:
            raise ValueError("Error: You can't play a 3 or 10 right after using a special card like 2 of spades or 9 of spades")
-            #For each round the sum of the values of the number cards played will be stored (5676101)
-        rank_user = user.get_rank(user_card)
+        #For each round the sum of the values of the number cards played will be stored (5676101)
         value_user = int(rank_user)
         self.user_round_sum += value_user
-        if self.user_round_sum in [12, 19]: # If statement used to check if sum is 12 or 19
+        if self.user_round_sum in [12, 19]: # If statement used to check if sum is 12 or 19 for player
            raise ValueError("Error: The sum of your played cards cannot be 12 or 19")
+        value_computer = int(rank_computer)
+        self.computer_round_sum += value_computer
+        if self.computer_round_sum in [12, 19]:
+            raise ValueError("I think a statement isn't needed here")
         for player in self.players: #Sum of 91 exception (5676101)
             if player.score >= 91:
-                print(f"{player.name} wins with 91 points!"
+                print(f"{player.name} wins with 91 points!")
+        # Comparing number
+         if value_user > value_computer:
+            print("You win this round!")
+            user.add_score(figure_value)
+        elif value_user < value_computer:
+            print("Computer wins this round!")
+            computer.add_score(figure_value)
+        # flip coin(else) I  use random I could make flip coin to decide winner.
+        else:
+            print(f"This round is a tie! Deciding winner by coin flip...")
+            winner = random.choice([self.players[0], self.players[1]])
+            winner.add_score(figure_value)  
+            print(f"{winner.name} wins the round by coin flip!")
     def game_over(self):
         """check if game completion conditions are satisfied(round expired or deck is empty)"""
         return self.round >= self.total_rounds or self.deck.is_empty()
