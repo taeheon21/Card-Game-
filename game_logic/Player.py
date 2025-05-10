5b4acc3d910b3dff5ab4c95bf3a9e3d6061efe74
Alexandru Pascal <Alexandru.Pascal@warwick.ac.uk>
2025-05-10 15:48:47 +0100

    Update Player.py

--- game_logic/Player.py
+++ game_logic/Player.py
@@ -1,8 +1,10 @@
 class Player: #5676101 (all of it, except def get_rank)
-
+     '''(5676101) I created this entire function besides def get_rank; Techniques used: Basic if/else conditionals;
+       data structure: list
+    '''
     #Represents a player in the card game and tracks the player's hand, score, and special abilities like skip turns.
-#For use when programming cpu as boolean (probably): #self.human = human  # Check whether this is a human or AI player
-    def __init__(self, name):
+
+    def __init__(self, name: str) -> None:
 
         self.name = name
         self.hand = []  # List of cards like '4 of spades'
@@ -11,7 +13,7 @@ class Player: #5676101 (all of it, except def get_rank)
         self.used_special = False  # Flag if player used a special card
         self.last_special = None  # The last special card played
 
-    def play_card(self, card):
+    def play_card(self, card: str) -> str:
         
         #Play a card from the player's hand
         
@@ -19,7 +21,7 @@ class Player: #5676101 (all of it, except def get_rank)
             self.hand.remove(card)
             return card
 
-    def skip_turn(self):
+    def skip_turn(self) -> bool:
 
         #Skip the current turn if skips are available. Will return trues if successfully skipped , false otherwise.
 
@@ -29,7 +31,7 @@ class Player: #5676101 (all of it, except def get_rank)
             return True
         return False
 
-    def add_score(self, value):
+    def add_score(self, value: int) -> int:
 
         #Add points to the player's score.
 
@@ -37,9 +39,12 @@ class Player: #5676101 (all of it, except def get_rank)
         print(f"{self.name} gained {value} points!")
         return value
 
-    def get_num(self, card):
+    def get_num(self, card: str) -> str:
         
-        #Extract the number from a card string. Example: From '10 of clubs' -> '10'
+        '''
+        Extract the number as a string from a card string. Example: From '10 of clubs' -> '10' .
+        It is later converted into an integer for comaprison in class Game.
+        '''
 
         return card[:-1]  
 
