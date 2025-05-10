de82a7cf318108d0224da7630ae53b9f26c9e439
taeheon21 <taeheon0121@gmail.com>
2025-05-10 16:47:41 +0100

    Update Player.py
    During the active fixing on UI is working, Alex made  unnecasssry  change to original player so UI doesn't work and fixing can't be done. So I restored the old player since Alex don't know how.

--- game_logic/Player.py
+++ game_logic/Player.py
@@ -1,9 +1,7 @@
 class Player: #5676101 (all of it, except def get_rank)
-     '''(5676101) I created this entire function besides def get_rank; Techniques used: Basic if/else conditionals;
-       data structure: list
-    '''
-    #Represents a player in the card game and tracks the player's hand, score, and special abilities like skip turns.
 
+    #Represents a player in the card game and tracks the player's hand, score, and special abilities like skip turns.
+#For use when programming cpu as boolean (probably): #self.human = human  # Check whether this is a human or AI player
     def __init__(self, name):
 
         self.name = name
@@ -13,7 +11,7 @@ class Player: #5676101 (all of it, except def get_rank)
         self.used_special = False  # Flag if player used a special card
         self.last_special = None  # The last special card played
 
-    def play_card(self, card: str) -> str:
+    def play_card(self, card):
         
         #Play a card from the player's hand
         
@@ -21,7 +19,7 @@ class Player: #5676101 (all of it, except def get_rank)
             self.hand.remove(card)
             return card
 
-    def skip_turn(self) -> bool:
+    def skip_turn(self):
 
         #Skip the current turn if skips are available. Will return trues if successfully skipped , false otherwise.
 
@@ -31,7 +29,7 @@ class Player: #5676101 (all of it, except def get_rank)
             return True
         return False
 
-    def add_score(self, value: int) -> int:
+    def add_score(self, value):
 
         #Add points to the player's score.
 
@@ -39,12 +37,9 @@ class Player: #5676101 (all of it, except def get_rank)
         print(f"{self.name} gained {value} points!")
         return value
 
-    def get_num(self, card: str) -> str:
+    def get_num(self, card):
         
-        '''
-        Extract the number as a string from a card string. Example: From '10 of clubs' -> '10' .
-        It is later converted into an integer for comaprison in class Game.
-        '''
+        #Extract the number from a card string. Example: From '10 of clubs' -> '10'
 
         return card[:-1]  
 
